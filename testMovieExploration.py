ae9dea1c31507453d9b37fa6f5ddccaa9aae1398
Zapp Brannigan <cjohhnso@gmail.com>
2017-11-13 14:10:13 -0500

    got groupby working with tuples that : after splitting string to list, then changing list to set to ensure no duplicates, then sort set, and return tuple (unmutable) to use with groupby...success!  Now to generalize this into a few functions and apply to actors and directors....

--- testMovieExploration.py
+++ testMovieExploration.py
@@ -15,8 +15,21 @@ df_movieMoney = pd.read_csv(filename, usecols=moneyFields)
 #https://stackoverflow.com/questions/15891038/change-data-type-of-columns-in-pandas
 df_movieMoney[['budget','revenue']] = df_movieMoney[['budget','revenue']].astype(float)
 #now try to change genres to sets to get better groupby performance
-df_commonFields['genres'] = df_commonFields.apply(lambda row: set(row['genres']),axis=1)
-#print df_movieMoney.head()
+#https://stackoverflow.com/questions/29530232/python-pandas-check-if-any-value-is-nan-in-dataframe
+#https://stackoverflow.com/questions/25326649/python-numpy-pandas-change-values-on-condition
+#df_commonFields.genres = df_commonFields.genres.where(df_commonFields.genres.isnull(),'')
+df_commonFields['genres'] = df_commonFields['genres'].astype(str)
+df_commonFields['genres'] = df_commonFields['genres'].str.split(pat='|')
+# print df_commonFields.genres.head()
+# print type(df_commonFields.genres.iloc[424])
+df_commonFields.genres = df_commonFields.genres.apply(set)
+print type(df_commonFields.genres.iloc[423])
+#need to convert from set to tuple b/c groupby can't work on mutable objects
+#https://stackoverflow.com/questions/39622884/pandas-groupby-over-list
+#now with set, create function to sort each set and create sorted tuple to feed into groupby to summarize genres correctly
+def setToTuple(x):
+    return tuple(sorted(x))
+df_commonFields.genres = df_commonFields.genres.apply(setToTuple)
 
 def rankByMoney(df):
     if (df['revenue'] > 1) and (df['budget'] > 100000):
@@ -41,12 +54,14 @@ print dfMerged.head()
 #print df_commonFields.head()
 #print df_commonFields[df_commonFields['id'] == 76341]
 
-print len(dfMerged)
-
-print df_topMovieMoney.columns
-print df_commonFields.columns
+# print len(dfMerged)
+#
+# print df_topMovieMoney.columns
+# print df_commonFields.columns
 
 print dfMerged.groupby(dfMerged['director']).groups
 #playing around with list of genre ranking
 #https://stackoverflow.com/questions/19384532/how-to-count-number-of-rows-in-a-group-in-pandas-group-by-object
+print type(dfMerged['genres'].iloc[424])
+#print dfMerged.groupby(dfMerged['genres']).groups
 print dfMerged.groupby(dfMerged['genres']).size().sort_values(ascending=False)
